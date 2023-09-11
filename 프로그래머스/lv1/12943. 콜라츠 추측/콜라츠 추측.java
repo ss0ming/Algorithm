f181c256d903bd7eb7926cc4efe3d1615c04400b
euniiiii <104823900+euniiiii@users.noreply.github.com>
2023-09-12 00:26:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        long n = (long)num;
+        
+        while (n > 1) {
+            if (answer > 500) {
+                answer = -1;
+                break;
+            }
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n*3 + 1;
+            }
+            answer++;
+        }
+        System.out.println(n);
+        
+        return answer;
+    }
+}
