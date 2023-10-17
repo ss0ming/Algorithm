d8b8180c496c56da4b3e82d7a44bdd8b6bf676fc
euniiiii <104823900+euniiiii@users.noreply.github.com>
2023-10-17 16:01:51 +0900

    [level 2] Title: 올바른 괄호, Time: 17.56 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(') {
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()) {
+                    answer = false;
+                    continue;
+                }
+                stack.pop();
+            }
+        }
+        
+        if (!stack.isEmpty()) answer = false;
+        
+        return answer;
+    }
+}
