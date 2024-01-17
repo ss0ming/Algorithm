feed61968e62a573bc1f76e442b14c5db1d59a9f
euniiiii <104823900+euniiiii@users.noreply.github.com>
2024-01-18 00:14:47 +0900

    [Bronze III] Title: 대지, Time: 304 ms, Memory: 27444 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int minX = 10000;
+        int maxX = -10000;
+        int minY = 10000;
+        int maxY = -10000;
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            maxX = Math.max(maxX, x);
+            minY = Math.min(minY, y);
+            maxY = Math.max(maxY, y);
+        }
+
+        int ans = (maxX - minX) * (maxY - minY);
+
+        System.out.println(ans);
+    }
+}
