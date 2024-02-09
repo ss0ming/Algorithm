fe1e8760b843e87f315e6f7d13698f9304a2783f
euniiiii <104823900+euniiiii@users.noreply.github.com>
2024-02-09 22:19:52 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            ans += (arr[i] * (n - i));
+        }
+
+        System.out.println(ans);
+    }
+}
