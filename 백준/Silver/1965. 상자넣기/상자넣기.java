24606fc457a6d0a00492740b0a0a266220701b2f
euniiiii <104823900+euniiiii@users.noreply.github.com>
2024-02-21 19:28:18 +0900

    [Silver II] Title: 상자넣기, Time: 144 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,33 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n];
+        int[] dp = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
+
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
-        }
 
-        int answer = 0;
-
-        for (int i=1; i<N; i++) {
-            for(int j=0; j<i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            answer = Math.max(dp[i], answer);
+            answer = Math.max(answer, dp[i]);
         }
 
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
