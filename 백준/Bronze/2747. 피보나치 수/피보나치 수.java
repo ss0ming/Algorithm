af20cdb7dcdee9359fe7e5150f123c4ff30d5591
euniiiii <104823900+euniiiii@users.noreply.github.com>
2024-03-15 14:19:45 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i=2; i<n+1; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
