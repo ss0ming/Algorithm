bb0aa264b4c9bd83b5cdd3ba9b9406316dac6e40
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-05-12 16:51:20 +0900

    [level 2] Title: 택배상자, Time: 161.84 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        int idx = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=1;i<=order.length;i++){
+            stack.push(i);
+            while(!stack.isEmpty()){
+                if(stack.peek()==order[idx]){
+                    stack.pop();
+                    idx++;
+                    answer++;
+                }
+                else break;
+            }
+        }
+        
+        return answer;
+    }
+}
