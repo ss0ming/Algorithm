26272a14520f5cae1dfca65afbc3b5791396689e
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-07-05 15:45:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,70 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int[][] arr;
+
+    static int N;
+    static String[][] board;
     static boolean[][] visited;
-    static List<Integer> list = new ArrayList<>();
-    static int[] dx = {-1, 0, 1, 0};
+    static List<Integer> answer = new ArrayList<>();
+    static int count = 0;
+    static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        visited = new boolean[n][n];
+        N = Integer.parseInt(br.readLine());
+        board = new String[N][N];
+        visited = new boolean[N][N];
 
-        for (int i=0; i<n; i++) {
-            String str = br.readLine();
-            for (int j=0; j<n; j++) {
-                arr[i][j] = str.charAt(j) - '0';
-            }
+        for (int i=0; i<N; i++) {
+            String line = br.readLine();
+            board[i] = line.split("");
         }
 
-        for (int i=0; i<n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] || arr[i][j] == 0) continue;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (board[i][j].equals("0") || visited[i][j]) continue;
                 visited[i][j] = true;
-                bfs(i, j);
+                answer.add(bfs(i, j));
             }
         }
 
-        System.out.println(list.size());
-        Collections.sort(list);
-        for (Integer i : list) {
+        Collections.sort(answer);
+        System.out.println(count);
+        for (Integer i : answer) {
             System.out.println(i);
         }
     }
 
-    public static void bfs(int x, int y) {
+    public static int bfs(int x, int y) {
+        int cnt = 1;
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x, y});
-        int cnt = 1;
 
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cn = cur[0];
-            int cy = cur[1];
-
+            int[] now = q.poll();
             for (int i=0; i<4; i++) {
-                int nx = cn + dx[i];
-                int ny = cy + dy[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
 
-                if (!isPossible(nx, ny) || visited[nx][ny]) continue;
-
-                visited[nx][ny] = true;
-                q.add(new int[]{nx, ny});
-                cnt++;
-
+                if (nx >= 0 && nx <N && ny >= 0 && ny < N && board[nx][ny].equals("1") && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
+                    cnt++;
+                }
             }
         }
 
-        list.add(cnt);
-    }
-
-    public static boolean isPossible(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < n && arr[x][y] == 1;
+        count++;
+        return cnt;
     }
 }
