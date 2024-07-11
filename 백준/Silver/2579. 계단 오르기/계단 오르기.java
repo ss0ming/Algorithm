eff4fb876f7302b80ccd47f5d6b09b68e82df1c4
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-07-12 00:31:48 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -9,22 +9,28 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N+1];
-        int[] DP = new int[N+1];
+        int[] dp = new int[N+1];
 
         for (int i=1; i<=N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        DP[1] = arr[1];
-
-        if (N >= 2) {
-            DP[2] = arr[1] + arr[2];
+        if (N == 1) {
+            System.out.println(arr[1]);
+            return;
         }
+        else if (N == 2) {
+            System.out.println(arr[1] + arr[2]);
+            return;
+        }
+
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
 
         for (int i=3; i<=N; i++) {
-            DP[i] = Math.max(DP[i-2], DP[i-3] + arr[i-1]) + arr[i];
+            dp[i] = Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
         }
 
-        System.out.println(DP[N]);
+        System.out.println(dp[N]);
     }
 }
