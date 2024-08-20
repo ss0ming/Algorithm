7c55b950fcef74bc7c31c9cf724808c2fc68b2ca
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-08-20 09:43:06 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,41 +3,42 @@ import java.util.*;
 
 public class Main {
 
+    static int[][] arr;
     static boolean[] visited;
     static int answer = 0;
-    static int[][] arr;
+    static int N;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int t = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        arr = new int[N+1][N+1];
+        visited = new boolean[N+1];
 
-        arr = new int[n+1][n+1];
-        visited = new boolean[n+1];
-
-        for (int i=0; i<t; i++) {
+        for (int i=0; i<M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
+
             arr[x][y] = 1;
             arr[y][x] = 1;
         }
 
         visited[1] = true;
         dfs(1);
-
         System.out.println(answer);
     }
 
-    public static void dfs(int now) {
-        for (int i=1; i<arr.length; i++) {
-            if (arr[now][i] == 1 && !visited[i]) {
-                visited[i] = true;
+    public static void dfs(int x) {
+        for (int i=1; i<=N; i++) {
+            if (arr[x][i] == 1 && !visited[i]) {
                 answer++;
+                visited[i] = true;
                 dfs(i);
             }
+
         }
     }
 }
