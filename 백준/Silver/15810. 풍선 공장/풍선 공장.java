d4b57f1e549bced25b5b67613993fb042169495c
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-08-23 09:54:26 +0900

    [Silver II] Title: 풍선 공장, Time: 940 ms, Memory: 92336 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] staff = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            staff[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(staff);
+
+        long left = 0;
+        long right = (long) staff[0] * M;
+
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+
+            for (int s : staff) {
+                count += mid / (long) s;
+            }
+
+            if (count >= M) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        System.out.println(left);
+    }
+
+}
