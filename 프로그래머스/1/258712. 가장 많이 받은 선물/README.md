aa1fe5f73ff63d12434455757f38f392495ea14c
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-09-13 01:25:29 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 50.06 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=java) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 4.04 ms
+메모리: 91.2 MB, 시간: 50.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 16:02:58
+2024년 09월 13일 01:25:24
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -4,52 +4,44 @@ class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
         
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> idx = new HashMap<>();
+        int[][] table = new int[friends.length][3];
+        int[][] record = new int[friends.length][friends.length];
         
         for (int i=0; i<friends.length; i++) {
-            map.put(friends[i], i);
+            idx.put(friends[i], i);
         }
-        
-        int[][] arr = new int[friends.length][friends.length];
         
         for (int i=0; i<gifts.length; i++) {
-            String[] strs = gifts[i].split(" ");
-            String a = strs[0];
-            String b = strs[1];
-            arr[map.get(a)][map.get(b)] += 1;     
+            String[] gift = gifts[i].split(" ");
+            int giveF = idx.get(gift[0]);
+            int receiveF = idx.get(gift[1]);
+            
+            table[giveF][0] += 1;
+            table[receiveF][1] += 1;
+            record[giveF][receiveF] += 1;
         }
-        
-        int[] jisu = new int[friends.length];
-        
-        for (int i=0; i<arr.length; i++) {
-            int giveGift = 0;
-            int recieveGift = 0;
-            for (int j=0; j<arr.length; j++) {
-                giveGift += arr[i][j];
-                recieveGift += arr[j][i];
-            }
-            jisu[i] = giveGift - recieveGift;
-        }
-        
-        int[] nextM = new int[friends.length];
         
         for (int i=0; i<friends.length; i++) {
-            for (int j=i+1; j<friends.length; j++) {
-                int a = arr[i][j];
-                int b = arr[j][i];
-                if (a > b) {
-                    nextM[i] += 1;
-                } else if (b > a) {
-                    nextM[j] += 1;
-                } else {
-                    if (jisu[i] > jisu[j]) nextM[i] += 1;
-                    else if (jisu[i] < jisu[j]) nextM[j] += 1;
+            table[i][2] = table[i][0] - table[i][1];
+            System.out.println(table[i][0] + ":" + table[i][1] + ":" + table[i][2]);
+        }
+        
+        for (int i=0; i<friends.length; i++) {
+            int cnt = 0;
+            for (int j=0; j<friends.length; j++) {
+                if (i == j) continue;
+                
+                if (record[i][j] > record[j][i]) {
+                    System.out.println("one: " + i + " " + j);
+                    cnt++;
+                } else if (record[i][j] == record[j][i] && table[i][2] > table[j][2]) {
+                    System.out.println("two: " + i + " " + j);
+                    cnt++;
                 }
             }
-        }
-        
-        for (int i=0; i<friends.length; i++) {
-            answer = Math.max(answer, nextM[i]);
+            System.out.println(cnt);
+            answer = Math.max(answer, cnt);
         }
         
         return answer;
