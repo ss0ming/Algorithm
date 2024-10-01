aef9ef7a1741f44833be90a378ed5a7d9c1d9445
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-10-01 18:19:47 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,17 +7,15 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[n+1];
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
 
-        for (int i=2; i<n+1; i++) {
+        for (int i=2; i<=N; i++) {
             dp[i] = dp[i-1] + 1;
-
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
             if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[N]);
     }
-
 }
