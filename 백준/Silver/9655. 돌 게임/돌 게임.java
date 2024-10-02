6ab010af5806391ea7a88c399ff434fd2bc8db89
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-10-02 09:41:47 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -3,15 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    static int[] arr;
-
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        if (n % 2 == 1) System.out.println("SK");
-        else System.out.println("CY");
+        if (N % 2 == 1) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
     }
 }
