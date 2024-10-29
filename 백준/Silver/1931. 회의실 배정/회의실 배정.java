16897a9503d6f2e79eb9d6e86aa694c4231cb298
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-10-29 21:20:22 +0900

    [Silver I] Title: 회의실 배정, Time: 480 ms, Memory: 44720 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] time = new int[N][2];
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 0;
+        int prevEndTime = 0;
+
+        for (int i=0; i<N; i++) {
+            if (time[i][0] >= prevEndTime) {
+                cnt++;
+                prevEndTime = time[i][1];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
