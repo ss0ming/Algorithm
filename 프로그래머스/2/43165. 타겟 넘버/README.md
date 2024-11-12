e9e4628b0f9435af3096abd5dfc0c8e0d0eaff1f
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-12 23:06:49 +0900

    [level 2] Title: 타겟 넘버, Time: 8.62 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.60 ms
+메모리: 92.4 MB, 시간: 8.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 21:26:43
+2024년 11월 12일 23:06:46
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,32 +1,24 @@
+import java.util.*;
+
 class Solution {
     
-    static int[] nums;
     static int answer = 0;
     static int t;
-    static int idx = 0;
     
     public int solution(int[] numbers, int target) {
-        
-        nums = new int[numbers.length];
         t = target;
         
-        for (int i=0; i<numbers.length; i++) {
-            nums[i] = numbers[i];
-        }
-        
-        dfs(0, 0);
-
-        System.out.println(idx);
+        dfs(0, 0, 0, numbers);
         return answer;
     }
     
-    public static void dfs (int v, int sum) {
-        if (v == nums.length) {
-            idx++;
-            if (sum == t) answer++;
+    public static void dfs(int idx, int sum, int cnt, int[] numbers) {
+        if (cnt == numbers.length) {
+            if (t == sum) answer++;
             return;
         }
-        dfs(v+1, sum+nums[v]);
-        dfs(v+1, sum-nums[v]);
+        
+        dfs(idx+1, sum+numbers[idx], cnt+1, numbers);
+        dfs(idx+1, sum-numbers[idx], cnt+1, numbers);
     }
 }
