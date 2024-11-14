54189767f85a85928feee3912fc767488dfe5a8c
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-14 16:24:24 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,35 @@
+import java.util.*;
+
 class Solution {
     
-    public static boolean[] visited;
-    public static int answer;
-    public static int[][] arr;
+    static int answer = 0;
+    static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
-        answer = 0;
-        
-        arr = new int[computers.length][computers[0].length];
         visited = new boolean[n];
-        
-        for (int i=0; i<computers.length; i++) {
-            for (int j=0; j<computers[i].length; j++) {
-                arr[i][j] = computers[i][j];
-            }
-        }
         
         for (int i=0; i<n; i++) {
             if (visited[i]) continue;
             visited[i] = true;
-            dfs(i);
+            bfs(i, computers);
             answer++;
         }
         
         return answer;
     }
     
-    public void dfs(int node) {
-        for (int i=0; i<arr[node].length; i++) {
-            if (visited[i] == true || arr[node][i] == 0) continue;
-            visited[i] = true;
-            dfs(i);
+    public static void bfs(int start, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i=0; i<computers.length; i++) {
+                if (cur != i && computers[cur][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                }
+            }
         }
     }
 }
