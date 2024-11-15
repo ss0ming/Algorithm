d7b112d674188c087776345d63df904c00a9839f
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-15 14:39:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.62 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 54.3 MB, 시간: 5.01 ms
+메모리: 54.4 MB, 시간: 10.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 18:56:10
+2024년 11월 15일 14:39:47
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,50 @@
 import java.util.*;
+
 class Solution {
     
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static int m;
-    static int n;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static int answer = -1;
-    static boolean[][] visited;
+    static int n;
+    static int m;
     
     public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
         
-        m = maps.length;
-        n = maps[0].length;
-        visited = new boolean[m][n];
-        visited[0][0] = true;
+        if (maps[0][0] == 0) return -1;
         
-        bfs(maps, visited);
-        
+        bfs(maps);
         return answer;
     }
     
-    public static void bfs(int[][] maps, boolean[][] visited) {
+    public static void bfs(int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
+        boolean[][] isVisited = new boolean[n][m];
         q.add(new int[]{0, 0, 1});
+        isVisited[0][0] = true;
         
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
-            int x = now[0];
-            int y = now[1];
-            int cnt = now[2];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
             
-            if (x == m-1 && y == n-1) {
-                answer = cnt;
-                break;
+            if (cur[0] == n-1 && cur[1] == m-1) {
+                answer = cur[2];
+                return;
             }
             
             for (int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (!isPromising(nx, ny) || maps[nx][ny] == 0 || visited[nx][ny]) continue;
-                visited[nx][ny] = true;
-                q.add(new int[]{nx, ny, cnt+1});
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if (isPromising(nx, ny) && !isVisited[nx][ny] && maps[nx][ny] == 1) {
+                    q.add(new int[]{nx, ny, cur[2] + maps[nx][ny]});
+                    isVisited[nx][ny] = true;
+                }
             }
         }
     }
     
     public static boolean isPromising(int x, int y) {
-        return x >= 0 && y >= 0 && x < m && y< n;
+        return x >= 0 && x < n && y >= 0 && y < m;
     }
 }
