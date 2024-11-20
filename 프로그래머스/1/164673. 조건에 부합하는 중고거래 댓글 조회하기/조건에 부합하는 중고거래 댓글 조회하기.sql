263a953ba791c46c5fae00917861ce8bd2b5cb4f
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-20 22:51:17 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT b.title,
+       b.board_id,
+       r.reply_id,
+       r.writer_id,
+       r.contents,
+       DATE_FORMAT(r.created_date, '%Y-%m-%d') as created_date
+FROM   used_goods_board b
+       JOIN used_goods_reply r
+         ON b.board_id = r.board_id
+WHERE b.created_date 
+    BETWEEN '2022-10-01' AND '2022-10-31'
+ORDER  BY created_date, title
