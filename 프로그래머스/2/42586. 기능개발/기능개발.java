ab6126c0a3eaac3891398fe42e7678087a8c74a0
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-25 20:08:35 +0900

    [level 2] Title: 기능개발, Time: 3.76 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,22 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-
-        Queue<Integer> stack = new LinkedList<>();
-
+        int[] days = new int[progresses.length];
+        Queue<Integer> q = new LinkedList<>();
+        
         for (int i=0; i<progresses.length; i++) {
-            if(stack.isEmpty()) {
-                stack.add(i);
-            } else if((int)Math.ceil((100.0 - progresses[stack.peek()]) / speeds[stack.peek()]) < (int)Math.ceil((100.0 - progresses[i]) / speeds[i])) {
-                answer.add(stack.size());
-                stack.clear();
-                stack.add(i);
+            days[i] = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0) days[i] += 1;
+        }
+        
+        for (int i=0; i<days.length; i++) {
+            if (q.isEmpty() || q.peek() >= days[i]) {
+                q.add(days[i]);
             } else {
-                stack.add(i);
+                answer.add(q.size());
+                q.clear();
+                q.add(days[i]);
             }
         }
-        if (!stack.isEmpty()) answer.add(stack.size());
         
-        return answer.stream().mapToInt(i->i).toArray();
+        if (!q.isEmpty()) answer.add(q.size());
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
