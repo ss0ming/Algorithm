2366e3aca5c3891a5d3df798712cf8c144681240
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-27 11:26:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.07 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,28 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        answer = participant[participant.length - 1];
-        
-        for (int i=0; i<completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                answer = participant[i];
-                break;
+        Map<String, Integer> map = new HashMap<>();
+        for (String p : participant) {
+            if (map.containsKey(p)) {
+                map.put(p, map.get(p)+1);
+            } else {
+                map.put(p, 1);
             }
+        }
+        
+        for (String c : completion) {
+            if (map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.replace(c, map.get(c)-1);
+            }
+        }
+        
+        for (String key : map.keySet()) {
+            answer = key;
         }
         
         return answer;
