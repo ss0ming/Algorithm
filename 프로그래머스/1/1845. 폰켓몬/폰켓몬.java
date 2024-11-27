e53cf5f1b96c66839f707501f7ee5f830d389132
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-11-27 10:34:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.54 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int N = nums.length/2;
+        int answer = nums.length/2;
         
         Set<Integer> set = new HashSet<>();
-        for (int i=0; i<nums.length; i++) {
-            set.add(nums[i]);
+        
+        for (int num : nums) {
+            set.add(num);
         }
         
-        if (set.size() <= N) {
-            return set.size();
+        if (set.size() < answer) {
+            answer = set.size();
         }
-
-        return N;
+        
+        return answer;
     }
 }
