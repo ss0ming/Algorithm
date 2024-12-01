cc9bedd5cf1cdf9be20fbd044970e2e58fa83c47
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-01 15:01:23 +0900

    [level 3] Title: 등굣길, Time: 1.04 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,30 +1,28 @@
+import java.util.*;
+
 class Solution {
+    
+    static final int MOD = 1_000_000_007;
+    
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        int[][] arr = new int[n+1][m+1];
+        int[][] dp = new int[n+1][m+1];
+        dp[1][1] = 1;
         
-        for (int i=1; i<n+1; i++) {
-            for (int j=1; j<m+1; j++) {
-                if (i == 1 && j == 1) {
-                    arr[i][j] = 1;
-                    continue;
+        for (int i=0; i<puddles.length; i++) {
+            dp[puddles[i][1]][puddles[i][0]] = -1;
+        }
+        
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (dp[i][j] != -1) {
+                    int a = dp[i-1][j] == -1 ? 0 : dp[i-1][j];
+                    int b = dp[i][j-1] == -1 ? 0 : dp[i][j-1];
+                    dp[i][j] += (a % MOD + b % MOD) % MOD;
                 }
-                if (isPuddle(i, j, puddles)) continue;
-                int left = arr[i][j-1];
-                int up = arr[i-1][j];
-                arr[i][j] = (left + up) % 1_000_000_007;
             }
         }
         
-        answer = arr[n][m] % 1_000_000_007;
-        
-        return answer;
-    }
-    
-    public static boolean isPuddle(int x, int y, int[][] puddles) {
-        for (int i=0; i<puddles.length; i++) {
-            if (puddles[i][0] == y && puddles[i][1] == x) return true;
-        }
-        return false;
+        return dp[n][m] % MOD;
     }
 }
