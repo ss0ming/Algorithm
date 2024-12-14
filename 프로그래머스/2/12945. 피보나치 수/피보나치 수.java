cc9ad93ab4f54e4eee318a2aeb6cecfb3a34f297
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-14 11:01:52 +0900

    [level 2] Title: 피보나치 수, Time: 2.50 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        dp[1] = 1;
         
-        arr[0] = 0;
-        arr[1] = 1;
-        
-        for (int i=2; i<arr.length; i++) {
-            arr[i] = arr[i-1] % 1234567 + arr[i-2] % 1234567;
+        for (int i=2; i<n+1; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
         }
         
-        answer = arr[n] % 1234567;
-        
-        return answer;
+        return dp[n];
     }
 }
