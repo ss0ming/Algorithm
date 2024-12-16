096de587ae0cc7b3c5159abaa3d5d0a5b6231b4b
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-16 11:07:57 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,29 +1,21 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        int w = 0;
         int h = 0;
         
-        for (int i=1; i<=Math.sqrt(yellow); i++) {
-            if (yellow % i != 0) continue;
-            w = i;
-            h = yellow/i;
+        while (h <= yellow) {
+            h++;
             
-            int tmp = (w+h) * 2 + 4;
+            if (yellow % h != 0) {
+                continue;
+            }
             
-            if (tmp == brown) break;
-        }
-        
-        w += 2;
-        h += 2;
-        
-        if (w > h) {
-            answer[0] = w;
-            answer[1] = h;
-        } else {
-            answer[0] = h;
-            answer[1] = w;
+            int w = yellow / h;
+            if ((w+h) * 2 == (brown - 4)) {
+                answer[0] = w+2;
+                answer[1] = h+2;
+                break;
+            }
         }
         
         return answer;
