1a660f4a2ce29a416eddc79916764e1ee5471dd5
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-16 11:52:07 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        
         int ans = 0;
         
-        while (true) {
-            if (n == 0) break;
-            
+        while (n >= 1) {
             if (n % 2 == 0) {
                 n /= 2;
-            } else {
-                n -= 1;
-                ans++;
+                continue;
             }
+            n--;
+            ans++;
         }
 
         return ans;
     }
-
 }
