6572ee6d34725e3af30837f28db63aa9d0a5f68b
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-22 22:33:24 +0900

    [level 2] Title: 할인 행사, Time: 24.94 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,33 +1,20 @@
-import java.util.*;
-
 class Solution {
-    
-    public static int answer = 0;
-    
     public int solution(String[] want, int[] number, String[] discount) {
-       
-        Map<String, Integer> wantList = new HashMap<>();
-        
-        for (int i=0; i<number.length; i++) {
-            wantList.put(want[i], number[i]);
-        }
-
-        for (int i=0; i<discount.length-10+1; i++) {
-            Map<String, Integer> discountList = new HashMap<>();
-            for (int j=i; j<i+10; j++) {
-                discountList.put(discount[j], discountList.getOrDefault(discount[j], 0) + 1);
+        int answer = 0;
+        for(int i=0;i<discount.length-9;i++) {
+            int j;
+            for(j=0;j<want.length;j++) {
+                int check=0;
+                for(int k=i;k<i+10;k++) {
+                    if(discount[k].equals(want[j])) check++;
+                }        
+                if(check<number[j]) {
+                     break;
+                }
             }
-            check(wantList, discountList);
+           if(j==want.length) answer ++;
         }
         
         return answer;
-    }
-    
-    public void check(Map<String, Integer> want, Map<String, Integer> discount) {
-        boolean bool = true;
-        for (String s : want.keySet()) {
-            if (!discount.containsKey(s) || want.get(s) > discount.get(s)) bool = false;
-        }
-        if (bool) answer++;
     }
 }
