edd205716aa1a105186d26e0c10841d1656a6972
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-24 20:14:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,18 +1,18 @@
 class Solution {
-    
     public long solution(int n) {
-        
-        long answer = 0;
-        if (n ==1) return 1;
-        long[] arr = new long[n+1];
-        
-        arr[1] = 1;
-        arr[2] = 2;
-        
-        for (int i=3; i<n+1; i++) {
-            arr[i] = (arr[i-2] + arr[i-1])%1234567;
+        if (n < 3) {
+            return n;
         }
         
-        return arr[n];
+        long[] dp = new long[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i=3; i<=n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
     }
 }
