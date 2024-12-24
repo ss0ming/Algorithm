55dafb36aa8a76f563b524043b2239ac129dfa15
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-24 20:55:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 103.07 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,59 +4,37 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
+        if (s.length() % 2 != 0) return 0;
+        
         for (int i=0; i<s.length(); i++) {
-            String first = s.substring(0, 1);
-            String last = s.substring(1);
-            s = last + first;
-            if (isRight(s)) answer++;
+            if (check(i, s)) answer++;
         }
         
         return answer;
     }
     
-    public boolean isRight(String s) {
+    private static boolean check(int start, String s) {
         Stack<Character> stack = new Stack<>();
         
-        boolean flag = true;
-        
         for (int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if (stack.isEmpty() && isOpenStr(c)) {
-                stack.add(c);
-            } else if (stack.isEmpty() && isCloseStr(c)) {
-                flag = false;
-            } else if (isOpenStr(c)) {
-                stack.add(c);
-            } else if (isCloseStr(c)) {
-                char topStack = stack.peek();
-                if (c == ')' && topStack == '(') {
-                    stack.pop();
-                } else if (c == ']' && topStack == '[') {
-                    stack.pop();
-                } else if (c == '}' && topStack == '{') {
-                    stack.pop();
-                } else {
-                    flag = false;
-                }
-            }
+            Character c = s.charAt((start + i) % s.length());
             
-            if (!flag) break;
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
         }
-        if (!stack.isEmpty()) flag = false;
-        return flag;
-    }
-    
-    public boolean isOpenStr(char c) {
-        if (c == '(') return true;
-        else if (c == '[') return true;
-        else if (c == '{') return true;
-        return false;
-    }
-    
-    public boolean isCloseStr(char c) {
-        if (c == ')') return true;
-        else if (c == ']') return true;
-        else if (c == '}') return true;
+        
+        if (stack.isEmpty()) return true;
         return false;
     }
 }
