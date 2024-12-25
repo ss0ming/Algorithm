c7f19e67cb735aa992e29ea4e9cfa2ec0a8bc139
ssoming <104823900+ss0ming@users.noreply.github.com>
2024-12-25 11:27:47 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.07 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#qna) 
 
 ### 성능 요약
 
-메모리: 89.2 MB, 시간: 4.45 ms
+메모리: 115 MB, 시간: 5.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 15:2:9
+2024년 12월 25일 11:27:44
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,13 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, long left, long right) {
-        
-        int[] answer = new int[(int)(right-left)+1];
+        int len = (int) (right-left+1);
+        int[] answer = new int[len];
         int idx = 0;
-
+        
         for (long i=left; i<=right; i++) {
-            answer[idx++] = Math.max((int)(i/n)+1, (int)(i%n)+1);
+            int a = (int) (i / n + 1);
+            int b = (int) (i % n + 1);
+            
+            if (a >= b) answer[idx] = a;
+            else answer[idx] = b;
+            idx++;
         }
         
         return answer;
