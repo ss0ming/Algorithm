049126666bf733e3fb6bbbf20561cc590d8c285e
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-01-03 19:45:35 +0900

    [level 2] Title: 롤케이크 자르기, Time: 92.04 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,38 +3,51 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
+        int[] leftSum = getLeftSum(topping);
+        int[] rightSum = getRightSum(topping);
         
-        Map<Integer, Integer> chul = new HashMap<>();
-        Map<Integer, Integer> dong = new HashMap<>();
-        
-        for (int i=0; i<topping.length; i++) {
-            int t = topping[i];
-            
-            if (dong.containsKey(t)) {
-                dong.replace(t, dong.get(t) + 1);
-            } else {
-                dong.put(t, 1);
+        for (int i=0; i<topping.length-1; i++) {
+            if (leftSum[i] == rightSum[i+1]) {
+                answer++;
             }
-        }
-        
-        for (int i=0; i<topping.length; i++) {
-            int t = topping[i];
-            
-            if (chul.containsKey(t)) {
-                chul.replace(t, chul.get(t) + 1);
-            } else {
-                chul.put(t, 1);
-            }
-            
-            if (dong.get(t) - 1 == 0) {
-                dong.remove(t);
-            } else {
-                dong.replace(t, dong.get(t) - 1);
-            }
-            
-            if (chul.size() == dong.size()) answer++;
         }
         
         return answer;
+    }
+    
+    private static int[] getLeftSum(int[] topping) {
+        Set<Integer> list = new HashSet<>();
+        list.add(topping[0]);
+        int[] sum = new int[topping.length];
+        sum[0] = 1;
+        
+        for (int i=1; i<topping.length; i++) {
+            if (list.contains(topping[i])) {
+                sum[i] = sum[i-1];
+            } else {
+                sum[i] = sum[i-1] + 1;
+                list.add(topping[i]);
+            }
+        }
+        
+        return sum;
+    }
+    
+    private static int[] getRightSum(int[] topping) {
+        Set<Integer> list = new HashSet<>();
+        list.add(topping[topping.length-1]);
+        int[] sum = new int[topping.length];
+        sum[topping.length-1] = 1;
+        
+        for (int i=topping.length-2; i>=0; i--) {
+            if (list.contains(topping[i])) {
+                sum[i] = sum[i+1];
+            } else {
+                sum[i] = sum[i+1] + 1;
+                list.add(topping[i]);
+            }
+        }
+        
+        return sum;
     }
 }
