7646366f48a913108e6e7a0c978f6891ec1e20bb
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-01-05 11:09:29 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 21.96 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#qna) 
 
 ### 성능 요약
 
-메모리: 87.7 MB, 시간: 4.07 ms
+메모리: 91.8 MB, 시간: 21.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 10:35:10
+2025년 01월 05일 11:09:27
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,32 +3,66 @@ import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        int hap = 0;
-        int cnt = 0;
-        
-        List<String> list = new LinkedList<>();
-            
-        for (int i=0; i<str1.length()-1; i++) {
-            String str = str1.substring(i, i+2).toLowerCase();
-            if (!str.matches("^[a-zA-Z]*$")) continue;
-            System.out.println(str);
-            list.add(str);
-            hap++;
-        }
-        for (int i=0; i<str2.length()-1; i++) {
-            String str = str2.substring(i, i+2).toLowerCase();
-            if (!str.matches("^[a-zA-Z]*$")) continue;
-            if (list.contains(str)) {
-                list.remove(str);
-                cnt++;
-            } else {
-                hap++;
-            }
-        }
-        System.out.println(cnt);
-        System.out.println(list.size());
-        answer = (int)((double)cnt / hap * 65536);
-        if (cnt ==0 && hap ==0) answer = 65536;
+
+        // 교집합의 크기 / 합집합의 크기
+        // 공집합인 경우 1
+        // 교집합 중복 허용 | 합집합은 중복 허용 x
+
+        // 1. 두 글자씩 끊어 다중 집합의 원소로 만들기
+        // 2. 공백, 숫자, 특수 문자가 들어있는 경우 글자 쌍 버린다.
+        // 대문자와 소문자의 차이는 무시한다.
+        // 최종 값 * 65536
+
+        List<String> arr1 = getArr(str1);
+        List<String> arr2 = getArr(str2);
+
+        answer = calculate(arr1,arr2);
+
         return answer;
+    }
+
+    public List<String> getArr(String str){
+
+        List<String> res = new ArrayList<>();
+
+        for(int i = 0; i < str.length()-1;i++){
+            String value = "" + str.charAt(i) + str.charAt(i+1);
+
+            if(value.matches("^[a-zA-Z]*$")){
+                res.add(value.toLowerCase());
+            }
+
+        }
+
+        return res;
+    }
+
+    public int calculate(List<String> arr1, List<String> arr2){
+        List<String> c = new ArrayList<>();
+        List<String> u = new ArrayList<>();
+
+        // 합집합 : A + B - A n B
+        // 교집합
+
+        for(String value : arr1){
+            if(arr2.contains(value)){
+                c.add(value);
+                arr2.remove(value);
+            }
+            u.add(value);
+        }
+
+        for(String value : arr2){
+            u.add(value);
+        }
+
+        int cNum = c.size();
+        int uNum = u.size();
+
+        if(u.size() == 0){
+            return 65536;
+        }
+
+        return (int)((double)cNum / uNum * 65536);
     }
 }
