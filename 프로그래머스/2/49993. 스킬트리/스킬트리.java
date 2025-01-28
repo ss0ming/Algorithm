412e32621fffe8b3c54b8e66f02b2eae938334b6
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-01-28 13:55:07 +0900

    [level 2] Title: 스킬트리, Time: 0.10 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,25 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
-        for (int i=0; i<skill_trees.length; i++) {
-            String skill_tree = skill_trees[i];
-            skill_tree = skill_tree.replaceAll("[^" + skill + "]", "");
-            
-            boolean bool = true;
-            
-            for (int j=0; j<skill_tree.length(); j++) {
-                if (skill.charAt(j) != skill_tree.charAt(j)) {
-                    bool = false;
-                    break;
-                }
+        for (String skill_tree : skill_trees) {
+            if (isValid(skill_tree, skill)) {
+                answer++;
             }
-            
-            if (bool) answer++;
         }
         
         return answer;
+    }
+    
+    private boolean isValid(String tree, String skill) {
+        int skillIdx = 0;
+        
+        for (char c : tree.toCharArray()) {
+            if (skill.contains(String.valueOf(c))) {
+                if (skill.charAt(skillIdx) == c) {
+                    skillIdx++;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
