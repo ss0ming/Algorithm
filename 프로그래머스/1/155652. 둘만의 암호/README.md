6f29764fbc397942726926eca5c87cbc95dc29a0
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-02-01 17:25:41 +0900

    [level 1] Title: 둘만의 암호, Time: 12.89 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#qna) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 1.79 ms
+메모리: 94 MB, 시간: 12.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 23일 13:17:49
+2025년 02월 01일 17:25:38
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,37 +1,21 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        
-    
-        List<Character> list = new LinkedList<>();
-        for (int i=0; i<skip.length(); i++) {
-            list.add(skip.charAt(i));
-        }
-        Character[] alpha = new Character[26];
-        
-        for (int i=0; i<s.length(); i++) {
-            char now = s.charAt(i);
-            if (alpha[now-97] != null) {
-                answer += alpha[now-97];
-                continue;
+
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            int stop = 0;
+
+            while (stop < index){
+                ch = (char) ((ch - 97 + 1) % 26 + 97);
+                if (!skip.contains(String.valueOf(ch))){
+                    stop++;
+                }
             }
-            alpha[now-97] = find(list, now, index);
-            answer += alpha[now-97];
+            answer += ch;
+
         }
-        
+
         return answer;
-    }
-    
-    public static Character find(List<Character> list, char now, int index) {
-        while (index != 0) {
-            now += 1;
-            if (now > 122) now = 97;
-            
-            if (!list.contains(now)) index--;
-        }
-        
-        return now;
     }
 }
