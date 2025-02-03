75ec69cf0885f8dd9fef2861a739ab18d660278f
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-02-03 23:35:14 +0900

    [level 1] Title: 과일 장수, Time: 11.61 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int[] sc2 = new int[k+1];
+        for(int i=0; i<score.length; i++) sc2[score[i]]++; 
+
+        int sum=0, mod=0;
+        for(int i=k; i>0; i--){         
+            sc2[i]=sc2[i] + mod;      
+            sum += sc2[i]/m * m * i;
+            mod = sc2[i]%m;    
+        }
+        return sum;
+    }
+}
