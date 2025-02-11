14b2e24704c794832c005b78e8232619c5d924ed
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-02-11 10:31:30 +0900

    [level 3] Title: 숫자 게임, Time: 62.81 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int idx = -1;
+        
+        for (int i=0; i<A.length; i++) {
+            while (idx < B.length-1) {
+                idx++;
+                if (A[i] < B[idx]) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
