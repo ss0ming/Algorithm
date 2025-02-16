acdff75c2cb0582a18b9f6ace6846be4cc4cc18b
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-02-16 18:03:17 +0900

    [level 2] Title: 오픈채팅방, Time: 138.12 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,33 +2,39 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-
-        String[] answer = {};
         
-        Map<String, String> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
+        Map<String, String> nicknameMap = new HashMap<>();
+        List<String[]> messages = new ArrayList<>();
         
-        for(int i = 0; i < record.length; i++){
-            String[] arr = record[i].split(" ");
+        for (int i=0; i<record.length; i++) {
+            String[] rec = record[i].split(" ");
+            String command = rec[0];
+            String id = rec[1];
+            String nickname = "";
             
-            if(arr[0].equals("Enter") || arr[0].equals("Leave")){
-                list.add(record[i]);
+            if (rec.length == 3) {
+                nickname = rec[2];
             }
             
-            if(arr.length > 2){
-                map.put(arr[1], arr[2]);
+            if (command.equals("Enter")) {
+                nicknameMap.put(id, nickname);
+                messages.add(new String[]{id, command});
+            } else if (command.equals("Leave")) {
+                messages.add(new String[]{id, command});
+            } else if (command.equals("Change")) {
+                nicknameMap.put(id, nickname);
             }
         }
         
-        answer = new String[list.size()];
-        
-        for(int i = 0; i < answer.length; i++){
-            String[] arr = list.get(i).split(" ");    
-            answer[i] = "" + map.get(arr[1]);
+        String[] answer = new String[messages.size()];
+        for (int i=0; i<answer.length; i++) {
+            String[] data = messages.get(i);
             
-            if(arr[0].equals("Enter")){
+            answer[i] = nicknameMap.get(data[0]);
+            
+            if (data[1].equals("Enter")) {
                 answer[i] += "님이 들어왔습니다.";
-            }else if(arr[0].equals("Leave")){
+            } else if (data[1].equals("Leave")) {
                 answer[i] += "님이 나갔습니다.";
             }
         }
