298f61a9cec22eccf8db6da0c9c13401b448e1f4
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-02-28 10:17:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 104.11 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,45 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        long s1  = 0;
-        long s2  = 0; 
-        ArrayDeque<int[]> q1 = new ArrayDeque<>();
-        ArrayDeque<int[]> q2 = new ArrayDeque<>();
-        for(int i = 0; i < queue1.length; i++){
-            q1.add(new int[]{i, queue1[i], 1});
-            q2.add(new int[]{i, queue2[i], 2});
-            s1 += queue1[i];
-            s2 += queue2[i];
-        }
-
         int answer = 0;
-        int cnt1 = 0;
-        int cnt2 = 0; 
         
-        while(true){
-            if(s1 == s2) return answer;
-            answer++;
-            if(s1 > s2){
-               int [] temp = q1.poll();
-               if(temp[0] == 0 && temp[2] == 1){
-                  cnt1++;
-                  if(cnt1 == 2) return -1;
-               }
-               s1 -= temp[1];
-               s2 += temp[1];
-               q2.add(temp); 
-            }
-            else{ // s1 < s2
-               int [] temp = q2.poll();
-               if(temp[0] == 0 && temp[2] == 2){
-                  cnt2++;
-                  if(cnt2 == 2)   return -1;
-               }
-               s2 -= temp[1];
-               s1 += temp[1];
-               q1.add(temp); 
-            }
+        long sum1 = 0L;
+        long sum2 = 0L;
+        
+        Queue<Integer> st1 = new LinkedList<>();
+        Queue<Integer> st2 = new LinkedList<>();
+        
+        for (int i=0; i<queue1.length; i++) {
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            st1.add(queue1[i]);
+            st2.add(queue2[i]);
         }
+        
+        if (sum1 == sum2) return 0;
 
+            
+        while (answer <= queue1.length*2+1 && !st1.isEmpty() && !st2.isEmpty()) {
+
+            int tmp = 0;
+            if (sum1 > sum2) {
+                tmp = st1.poll();
+                st2.offer(tmp);
+                sum1 -= tmp;
+                sum2 += tmp;
+            } else if (sum1 < sum2) {
+                tmp = st2.poll();
+                st1.offer(tmp);
+                sum1 += tmp;
+                sum2 -= tmp;
+            }
+            answer++;
+            
+            if (sum1 == sum2) return answer;
+        }
+        
+        return -1;
     }
 }
