359964c13cdcf60daf3c56e5947643986052fe2a
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-04-19 21:36:04 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 40.77 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 96.6 MB, 시간: 15.49 ms
+메모리: 102 MB, 시간: 40.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 23:03:01
+2025년 04월 19일 21:36:01
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,21 +1,43 @@
+import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = 0;        
-        int deliver = 0;        
-        int pickup = 0;       
+        long answer = 0;
+        int dIdx = n - 1;
+        int pIdx = n - 1;
         
-        for(int i = n-1; i >= 0; i--){            
-            deliver += deliveries[i];            
-            pickup += pickups[i];     
+        while (dIdx >= 0 || pIdx >= 0) {
+            while (dIdx >= 0 && deliveries[dIdx] == 0) dIdx--;
+            while (pIdx >= 0 && pickups[pIdx] == 0) pIdx--;
+            if (dIdx < 0 && pIdx < 0) break;
+
+            answer += (Math.max(dIdx, pIdx) + 1) * 2;
+ 
+            int dCap = cap;
+            while (dIdx >= 0 && dCap > 0) {
+                if (deliveries[dIdx] <= dCap) {
+                    dCap -= deliveries[dIdx];
+                    deliveries[dIdx] = 0;
+                    dIdx--;
+                } else {
+                    deliveries[dIdx] -= dCap;
+                    dCap = 0;
+                }
+            }
             
-            while (deliver > 0 || pickup > 0){                
-                deliver -= cap;                
-                pickup -= cap;                
-                answer += (i+1) * 2;            
-            }         
-        }  
+            int pCap = cap;
+            while (pIdx >= 0 && pCap > 0) {
+                if (pickups[pIdx] <= pCap) {
+                    pCap -= pickups[pIdx];
+                    pickups[pIdx] = 0;
+                    pIdx--;
+                } else {
+                    pickups[pIdx] -= pCap;
+                    pCap = 0;
+                }
+            }
+        }
         
         return answer;
-
     }
 }
