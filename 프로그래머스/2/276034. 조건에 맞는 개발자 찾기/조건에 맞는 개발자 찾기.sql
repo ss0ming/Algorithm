06fa222996be174b9b1b35af35ffc9d44ba09178
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-05-20 22:06:14 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,15 @@
+-- 코드를 작성해주세요
+SELECT ID
+      ,EMAIL
+      ,FIRST_NAME
+      ,LAST_NAME 
+FROM DEVELOPERS 
+WHERE SKILL_CODE & (
+    SELECT CODE 
+    FROM SKILLCODES
+    WHERE NAME IN ('C#')) > 0 
+ OR SKILL_CODE & (
+    SELECT CODE 
+    FROM SKILLCODES
+    WHERE NAME IN ('Python')) > 0 
+ORDER BY ID
