e604aab741f09f7f3cdf1868e5c3260a3703a764
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-05-21 10:17:01 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,23 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int storey) {
+        // 5 이하면 10 나누고 나머지 더하고
+        // 5 이상이면 10 나누고 하나 더하고 10-현재 숫자 더하고
         int answer = 0;
         
-        while (storey != 0) {
-			int one = storey % 10; 
-			int ten = (storey / 10) % 10; 
-
-			if (one > 5) { 
-				answer += (10 - one);
-				storey += 10;
-			} else if (one == 5) {
-				answer += one;
-				storey += (ten < 5 ? 0 : 10);
-			} else { 
-				answer += one;
-			}
-
-			storey /= 10;
-		}
+        while (storey > 0) {
+            int tmp = storey % 10;
+            storey /= 10;
+            
+            if (tmp < 5) {
+                answer += tmp;
+            } else if (tmp > 5) {
+                answer += (10 - tmp);
+                storey++;
+            } else {
+                if (storey % 10 >= 5) {
+                    answer += 10- tmp;
+                    storey++;
+                } else {
+                    answer += tmp;
+                }
+            }
+        }
+        
         return answer;
     }
 }
