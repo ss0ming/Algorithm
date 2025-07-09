8beeaa8214aa97245e4122c7856cf1867267269a
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-07-09 09:50:25 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.64 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/1/118666. 성격 유형 검사하기/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 16.47 ms
+메모리: 84.7 MB, 시간: 3.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 20:04:47
+2025년 07월 09일 09:50:21
 
 ### 문제 설명
 
@@ -127,11 +127,12 @@
 <li><code>survey[i]</code>의 첫 번째 캐릭터는 i+1번 질문의 비동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다. </li>
 <li><code>survey[i]</code>의 두 번째 캐릭터는 i+1번 질문의 동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다.</li>
 </ul></li>
-<li><p><code>choices</code>의 길이 = <code>survey</code>의 길이</p>
+<li><code>choices</code>의 길이 = <code>survey</code>의 길이
 
 <ul>
 <li><code>choices[i]</code>는 검사자가 선택한 i+1번째 질문의 선택지를 의미합니다.</li>
 <li>1 ≤ <code>choices</code>의 원소 ≤ 7</li>
+</ul></li>
 </ul>
 <table class="table">
         <thead><tr>
@@ -168,9 +169,7 @@
 <td>매우 동의</td>
 </tr>
 </tbody>
-      </table></li>
-</ul>
-
+      </table>
 <hr>
 
 <h5>입출력 예</h5>
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,28 +3,50 @@ import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        Map<Character, Integer> map = new HashMap<>();
         init(map);
         
         for (int i=0; i<survey.length; i++) {
-            int choice = choices[i]; 
-            if (choice < 4) {
-                map.replace(survey[i].charAt(0), map.get(survey[i].charAt(0)) - (choice-4));
-            } else if (choice > 4) {
-                map.replace(survey[i].charAt(1), map.get(survey[i].charAt(1)) + (choice-4));
+            int choice = choices[i];
+            if (choice == 4) {
+                continue;
+            } else if (choice < 4) {
+                char type = survey[i].charAt(0);
+                map.put(type, map.get(type) + Math.abs(choice - 4));
+            } else {
+                char type = survey[i].charAt(1);
+                map.put(type, map.get(type) + choice - 4);
             }
         }
         
-        answer += selectType('R', 'T', map);
-        answer += selectType('C', 'F', map);
-        answer += selectType('J', 'M', map);
-        answer += selectType('A', 'N', map);
+        if (map.get('R') >= map.get('T')) {
+            answer += 'R';
+        } else {
+            answer += 'T';
+        }
+        
+        if (map.get('C') >= map.get('F')) {
+            answer += 'C';
+        } else {
+            answer += 'F';
+        }
+        
+        if (map.get('J') >= map.get('M')) {
+            answer += 'J';
+        } else {
+            answer += 'M';
+        }
+        
+        if (map.get('A') >= map.get('N')) {
+            answer += 'A';
+        } else {
+            answer += 'N';
+        }
         
         return answer;
     }
     
-    public static void init(Map<Character, Integer> map) {
+    private void init(Map<Character, Integer> map) {
         map.put('R', 0);
         map.put('T', 0);
         map.put('C', 0);
@@ -33,10 +55,5 @@ class Solution {
         map.put('M', 0);
         map.put('A', 0);
         map.put('N', 0);
-    }
-    
-    public static char selectType(char one, char two, Map<Character, Integer> map) {
-        if (map.get(one) < map.get(two)) return two;
-        return one;
     }
 }
