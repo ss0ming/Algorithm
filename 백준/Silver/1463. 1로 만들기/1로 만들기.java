65dafe3f3f0e528e249aae8f588368b6681d2dae
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-08-04 10:57:06 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+1];
 
-        for (int i=2; i<=N; i++) {
-            dp[i] = dp[i-1] + 1;
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
-        }
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
+        while (!q.isEmpty()) {
+            int cur = q.poll();
 
-        System.out.println(dp[N]);
+            if (cur == 1) {
+                System.out.println(dp[1]);
+                break;
+            }
+
+            if (cur % 3 == 0 && cur/3 > 0 && dp[cur/3] == 0) {
+                dp[cur/3] = dp[cur] + 1;
+                q.add(cur/3);
+            }
+
+            if (cur % 2 == 0 && cur/2 > 0 && dp[cur/2] == 0) {
+                dp[cur/2] = dp[cur] + 1;
+                q.add(cur/2);
+            }
+
+            if (cur-1 > 0 && dp[cur-1] == 0) {
+                dp[cur-1] = dp[cur] + 1;
+                q.add(cur-1);
+            }
+        }
     }
 }
