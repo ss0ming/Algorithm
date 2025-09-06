09a9ebb55dc6048a3044c47cde304021958b801f
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-09-06 16:28:07 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.85 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,30 +3,32 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        Stack<Integer> basket = new Stack<>();
         
-        Stack<Integer> q = new Stack<>();
+        Map<Integer, Stack<Integer>> map = new HashMap<>();
+        for (int j=0; j<board[0].length; j++) {
+            map.put(j+1, new Stack<>());
+            Stack tmp = map.get(j+1);
+            for (int i=board.length-1; i>=0; i--) {
+                if (board[i][j] == 0) continue;
+                tmp.add(board[i][j]);
+            }
+        }
         
         for (int i=0; i<moves.length; i++) {
-            int n = moves[i] - 1;
+            int m = moves[i];
+            Stack s = map.get(m);
+            if (s.isEmpty()) continue;
+            int pick = (int) s.pop();
             
-            for (int j=0; j<board.length; j++) {
-                int now = board[j][n];
-                
-                if (now != 0) {
-                    
-                    if (q.isEmpty()) {
-                        q.add(now);
-                    } else if (q.peek() != now) {
-                        q.add(now);
-                    } else if (q.peek() == now) {
-                        q.pop();
-                        answer += 2;
-                    }
-                    
-                    
-                    
-                    board[j][n] = 0;
-                    break;
+            if (basket.isEmpty()) {
+                basket.add(pick);
+            } else {
+                if (basket.peek() == pick) {
+                    basket.pop();
+                    answer += 2;
+                } else {
+                    basket.add(pick);
                 }
             }
         }
