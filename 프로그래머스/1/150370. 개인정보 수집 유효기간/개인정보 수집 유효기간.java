77e681f8c8742aa41c3f520ff48222c99cf72185
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-10-06 12:21:01 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 14.21 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,76 +1,60 @@
 import java.util.*;
 
 class Solution {
+    
+    static Map<String, Integer> termMap = new HashMap<>();
+    static List<Integer> answer = new ArrayList<>();
+    
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        String[][] expired = new String[privacies.length][2];
-        Map<Character, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<terms.length; i++) {
-            String[] tmp = terms[i].split(" ");
-            map.put(tmp[0].charAt(0), Integer.parseInt(tmp[1]));
-        }
+        int todayNum = strToInt(today);
 
+        for (int i=0; i<terms.length; i++) {
+            String[] split = terms[i].split(" ");
+            termMap.put(split[0], Integer.parseInt(split[1]));
+        }
+        
         for (int i=0; i<privacies.length; i++) {
-            String[] tmp = privacies[i].split(" ");
+            String[] split = privacies[i].split(" ");
+            int p = addTerms(split[0], termMap.get(split[1]));
             
-            expired[i][0] = Integer.toString(i);
-            expired[i][1] = calculate(map.get(tmp[1].charAt(0)), tmp[0]);
-
-        }
-        
-        Arrays.sort(expired, new Comparator<>() {
-           @Override
-            public int compare(String[] o1, String[] o2) {
-                return o1[1].compareTo(o2[1]);
+            if (p <= todayNum) {
+                answer.add(i+1);
             }
-        });
-        
-        today = today.replace(".", "");
-        for (int i=0; i<expired.length; i++) {
-            if (Integer.parseInt(today) <= Integer.parseInt(expired[i][1])) break; 
-            answer.add(Integer.parseInt(expired[i][0])+1);
         }
         
-        Collections.sort(answer);
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    private String calculate(int term, String date) {
-        String[] splitDate = date.split("\\.");
+    private static int addTerms(String str, int n) {
+        String[] split = str.split("\\.");
         
-        int y = Integer.parseInt(splitDate[0]);
-        int m = Integer.parseInt(splitDate[1]);
-        int d = Integer.parseInt(splitDate[2]);
-        
-        m += term;
-        if (m > 12) {
-            y += (m / 12);
-            m = m - (12 * (m / 12));
+        int y = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+        int d = Integer.parseInt(split[2]);
+
+        if (n < 12) {
+            m += n;
+            if (m > 12) {
+                y += 1;
+                m -= 12;
+            }
+        } else if (n == 12) {
+            y += 1;
+        } else {
+            y += n/12;
+            m += n%12;
+            if (m > 12) {
+                y += 1;
+                m -= 12;
+            }
         }
         
-        d -= 1;
-        if (d == 0) {
-            d = 28;
-            m -= 1;
-        }
-        if (m == 0) {
-            m = 12;
-            y -= 1;
-        }
-        
-        String res = Integer.toString(y);
-        res += dateToString(m);
-        res += dateToString(d);
-        
-        return res;
+        String result = String.format("%02d%02d%02d", y, m, d);
+        return Integer.parseInt(result);
     }
     
-    private String dateToString(int d) {
-        String res = Integer.toString(d);
-        if (res.length() == 1) {
-            return "0" + res;
-        }
-        return res;
+    private static int strToInt(String str) {
+        str = str.replace(".", "");
+        return Integer.parseInt(str);
     }
 }
