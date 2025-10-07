d738ea8c35e85b48fc4035318450ec821550a4e4
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-10-07 22:03:24 +0900

    [level 2] Title: 주차 요금 계산, Time: 9.10 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
 
 ### 성능 요약
 
-메모리: 110 MB, 시간: 40.31 ms
+메모리: 92.4 MB, 시간: 9.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 10일 21:25:34
+2025년 10월 07일 22:03:21
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,37 +1,63 @@
 import java.util.*;
 
 class Solution {
+    
+    static List<String> carList = new ArrayList<>();
+    static Map<String, String> inCar = new HashMap<>(); // 입차한 차들
+    static Map<String, Integer> time = new HashMap<>(); // 시간 기록
+    static Map<String, Integer> fee = new HashMap<>(); // 요금 기록
+    
     public int[] solution(int[] fees, String[] records) {
-        Map<String, Integer> tmp = new HashMap<>();
-        Map<String, Integer> answer = new HashMap<>();
         
+        // 입차 출차 기록
         for (int i=0; i<records.length; i++) {
-            String[] record = records[i].split(" ");
+            String[] split = records[i].split(" ");
+            String t = split[0];
+            String carNum = split[1];
+            String inout = split[2];
             
-            if (record[2].equals("IN")) {
-                tmp.put(record[1], hToM(record[0]));
-            } else {
-                int cal = hToM(record[0]) - tmp.get(record[1]);
-                tmp.remove(record[1]);
-                answer.put(record[1], answer.getOrDefault(record[1], 0) + cal);
+            if (inout.equals("IN")) { // 입차일 때
+                inCar.put(carNum, t);
+            } else { // 출차일 때
+                String inTime = inCar.get(carNum);
+                int d = strToTime(t) - strToTime(inTime);
+                time.put(carNum, time.getOrDefault(carNum, 0) + d);
+                inCar.remove(carNum);
             }
         }
         
-        List<String> keySet = new ArrayList<>(answer.keySet());
-        Collections.sort(keySet);
-        
-        List<Integer> result = new ArrayList<>();
-        
-        for (String key : keySet) {
-            int price = 0;
-
+        for (String s : inCar.keySet()) {
+            String t = inCar.get(s);
+            int d = strToTime("23:59") - strToTime(t);
+            time.put(s, time.getOrDefault(s, 0) + d);
         }
         
-        return result;
+        int[] answer = new int[time.size()];
+        for (String s : time.keySet()) {
+            carList.add(s);
+            int t = time.get(s);
+            
+            if (t <= fees[0]) {
+                fee.put(s, fees[1]);
+            } else {
+                int f = (int) Math.ceil((double)(t - fees[0])/fees[2]) * fees[3];
+                fee.put(s, fees[1] + f);
+            }
+        }
+        
+        Collections.sort(carList);
+        for (int i=0; i<carList.size(); i++) {
+            answer[i] = fee.get(carList.get(i));
+        }
+        
+        return answer;
     }
     
-    private static int hToM(String time) {
-        String[] t = time.split(":");
-        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    private static int strToTime(String s) {
+        String[] split = s.split(":");
+        int h = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+        
+        return h * 60 + m;
     }
 }
