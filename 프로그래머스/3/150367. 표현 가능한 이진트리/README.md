912efa1a29e7ac28989f84ef9d2a2cb1ae996512
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-10-07 15:06:09 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 111.80 ms, Memory: 227 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 64.19 ms
+메모리: 227 MB, 시간: 111.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 21일 13:37:19
+2025년 10월 07일 15:06:06
 
 ### 문제 설명
 
--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -2,59 +2,37 @@ class Solution {
     public int[] solution(long[] numbers) {
         int[] answer = new int[numbers.length];
         
-        for (int i=0; i<numbers.length; i++) {
-            String nb = Long.toBinaryString(numbers[i]);
-            int len = nb.length();
-            int n = 1;
-            while (true) {
-                if (Math.pow(2, n)-1 >= len) {
-                    break;
-                }
-                n++;
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            String bin = Long.toBinaryString(numbers[i]);
             
-            for (int z=0; z<Math.pow(2, n)-1-len; z++) {
-                nb = "0" + nb;
+            // (1) 트리 구조를 위한 길이 보정 (2^n - 1)
+            int size = 1;
+            while (size < bin.length()) {
+                size = size * 2 + 1;
             }
+            bin = String.format("%" + size + "s", bin).replace(' ', '0');
             
-            boolean res = check(nb);
-            if (res) {
+            // (2) 재귀 검증
+            if (check(bin)) {
                 answer[i] = 1;
-            } else {
-                answer[i] = 0;
             }
-        }        
-
+        }
         return answer;
     }
     
-    private boolean check(String n) {
-        if (n.length() == 0) return true;
-        int mid = n.length() / 2;
-        char root = n.charAt(mid);
+    private static boolean check(String s) {
+        if (s.length() <= 1) return true;
+        int mid = s.length() / 2;
         
-        String left = n.substring(0, mid);
-        String right = n.substring(mid+1);
+        char root = s.charAt(mid);
+        String left = s.substring(0, mid);
+        String right = s.substring(mid + 1);
         
-        if (root == '0') {
-            return isZero(left) && isZero(right);
-        }
-        
-        return check(left) && check(right);
-    }
-    
-    private boolean isZero(String n) {
-        if (n.length() == 0) return true;
-        int mid = n.length() / 2;
-        char root = n.charAt(mid);
-        
-        String left = n.substring(0, mid);
-        String right = n.substring(mid+1);
-        
-        if (root == '1') {
+        // 부모가 0인데, 자식 중 하나라도 1이면 불가능
+        if (root == '0' && (left.charAt(left.length() / 2) == '1' || right.charAt(right.length() / 2) == '1')) {
             return false;
         }
         
-        return isZero(left) && isZero(right);
+        return check(left) && check(right);
     }
 }
