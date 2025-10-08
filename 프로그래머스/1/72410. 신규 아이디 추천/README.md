3184fa8be46ff0ab0430a6e932bc8a8331496a98
ssoming <104823900+ss0ming@users.noreply.github.com>
2025-10-08 15:11:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.57 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 1.59 ms
+메모리: 81.3 MB, 시간: 12.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 05일 22:46:59
+2025년 10월 08일 15:11:22
 
 ### 문제 설명
 
--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,41 +1,33 @@
-import java.util.*;
-
 class Solution {
     public String solution(String new_id) {
-        StringBuilder sb = new StringBuilder(new_id);
-
-        // 1. 대문자 -> 소문자
-        for (int i = 0; i < sb.length(); i++) {
-            char c = sb.charAt(i);
-            if (Character.isUpperCase(c)) sb.setCharAt(i, Character.toLowerCase(c));
+        String answer = new_id;
+        
+        answer = answer.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9._-]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        if (answer.charAt(0) == '.') {
+            answer = answer.substring(1);
+        }
+        if (answer.length() > 0 && answer.charAt(answer.length()-1) == '.') {
+            answer = answer.substring(0, answer.length()-1);
         }
 
-        // 2. 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자 제거
-        for (int i = sb.length() - 1; i >= 0; i--) {
-            char c = sb.charAt(i);
-            if (c == '-' || c == '_' || c == '.' || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9')) continue;
-            sb.deleteCharAt(i);
+        if (answer.isEmpty()) answer = "a";
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            if (answer.length() > 0 && answer.charAt(answer.length()-1) == '.') {
+                answer = answer.substring(0, answer.length()-1);
+            }
         }
+        
 
-        // 3. .. → .
-        sb = new StringBuilder(sb.toString().replaceAll("[.]{2,}", "."));
-
-        // 4. 마침표(.)가 맨 앞이거나 맨 뒤에 있다면 제거
-        if (sb.length() > 0 && sb.charAt(0) == '.') sb.deleteCharAt(0);
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') sb.setLength(sb.length() - 1);
-
-        // 5. 빈 문자열이라면 "a" 삽입
-        if (sb.length() == 0) sb.append('a');
-
-        // 6. 길이가 16 이상이면 16번째부터 끝까지 제거
-        if (sb.length() > 15) sb.setLength(15);
-
-        // 6-1. 마침표(.)가 맨 뒤에 있다면 제거
-        while (sb.charAt(sb.length() - 1) == '.') sb.setLength(sb.length() - 1);
-
-        // 7. 길이가 2 이하라면 마지막 문자를 길이가 3이 될 때까지 삽입
-        while (sb.length() < 3) sb.append(sb.charAt(sb.length() - 1));
-
-        return sb.toString();
+        if (answer.length() <= 2) {
+            char c = answer.charAt(answer.length()-1);
+            while (answer.length() < 3) {
+                answer += c;
+            }
+        }
+        
+        return answer;
     }
 }
